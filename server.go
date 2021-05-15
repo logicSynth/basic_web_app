dffe3936ad154d12ea0a9273a44bedb409a1a07e
victor UDOH <vicudoh345@gmail.com>
2021-05-15 10:37:01 +0100

    edit server.go

--- server.go
+++ server.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 )
 
-//handle function to process request response events
-func handler(w http.ResponseWriter, r *http.Request) {
+/* creat a handler to handle function to process
+request response events */
+func index_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world, this is Go")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", index_handler)
+	/* create a request listener: that listen to  a port
+	and passes connection to a port */
 	http.ListenAndServe(":8080", nil)
 }
